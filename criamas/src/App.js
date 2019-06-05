e276e3499062d3c9f9ef96309051d4640f48b281
Osmary Carrillo <osmarycarrillo@gmail.com>
2019-06-05 12:51:15 -0300

    create Bslider component

--- criamas/src/App.js
+++ criamas/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Bslider from './Bslider';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Bslider></Bslider>
         <p>
           SKY CODE WORKING
         </p>
+        
         <h1> Enjoy Learning </h1>
 
         <p>Team:<b>Osmary Carrillo, Adriana Montilla, Lourdes Rosales y Mercedes Ventocilla</b></p>
 
       </header>
+
     </div>
   );
 }
---
+++ criamas/src/Bslider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Bslider extends React.Component {
+  
+
+  render () {
+    return (
+      <div>
+        <span>Hola!, soy un componente</span>
+      </div>
+    )
+  }
+}
+export default Bslider;
