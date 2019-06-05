a2b4aae3ec1e56e39f15403ae46fb138c9450599
mechiventocilla87 <mercedesventocilla@gmail.com>
2019-06-05 10:12:47 -0300

    by meche

--- criamas/src/App.js
+++ criamas/src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>
           SKY CODE WORKING
         </p>
         <p>Team:<b>Osmary Carrillo, Adriana Montilla, Lourdes Rosales y Mercedes Ventocilla</b></p>
-        
+
       </header>
     </div>
   );
