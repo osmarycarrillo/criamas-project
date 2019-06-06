51ea1a34bc03116fe46e430ae1b9e6fd423f151f
Adriana M <adrianaymontilla@gmail.com>
2019-06-06 15:27:45 -0300

    add minitext

---
+++ criamas/src/Textb.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class Textb extends Component {
+
+    render() {
+        return (
+            <div className="box-p">
+            <p className="red-trans"> Bienvenido a Traslados</p>
+            <h2>Aquí encontrará información</h2>
+            <h2>sobre nuestro servicio</h2>
+            <p>Ante cualquier duda,podemos ayudarte</p>
+      
+
+            </div>
+        )
+    }
+}
+export default Textb;
+// Este componente realiza la descripcion del texto del banner
+// que pertenene a  las vistas  de traslados,blog,detalle del producto y detalle de cachorros AM
+
