c044408feca5960cd6a77d725debd321349582af
Adriana M <adrianaymontilla@gmail.com>
2019-06-06 19:33:37 -0300

    add text static

--- criamas/src/Bstatic.js
+++ criamas/src/Bstatic.js
@@ -7,10 +7,12 @@ class Bstatic extends React.Component {
     render() {
         return (
             <div className="translates" >
+                <div className="cont-trans">
               {/* dentro de las llaves iria el nombre de la ruta en la que este parado el usuario */}
                 <Textb>{}</Textb>
                 <div className="dogt"> <img src="" alt="perro imagen para traslados"></img> </div>
                 <div className="square"></div>
+                </div>
             </div>
         )
     }
--- criamas/src/Textb.js
+++ criamas/src/Textb.js
@@ -11,8 +11,19 @@ class Textb extends Component {
             <p className="tinfo">sobre nuestro servicio</p>
             <p>Ante cualquier duda, podemos ayudarte.</p>
       
-
-            </div>
+            <p className="red-trans"> Bienvenido a Nuestro Blog</p>
+            <p className="tinfo">Aquí encontrará información útil</p>
+            <p>Ante cualquier duda, podemos ayudarte.</p>
+{/* en esta seccion debe ir un bread crum que traiga el nombre 
+del producto el contenido es dinamico */}
+            <p className="tinfo">Detalle del producto</p>
+            <p>Pet shop/bolsa blanca.</p>
+           
+{/* // requerimos decidir que caracteristicas del cachorro se van a mostrar en esta seccion 
+// tambien es dinamico y va casado al filtro */}
+<p className="tinfo">caracteristicas de nuestro cachorro</p>
+<p>Ante cualquier duda, podemos ayudarte.</p>
+</div>
         )
     }
 }
