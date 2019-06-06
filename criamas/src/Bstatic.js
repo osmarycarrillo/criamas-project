b47fef64b3609ca62c3166fecbb6eb08f8061f25
Adriana M <adrianaymontilla@gmail.com>
2019-06-06 15:26:27 -0300

    import text

--- criamas/src/Bstatic.js
+++ criamas/src/Bstatic.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Textb from './Textb';
 
-class Bstatic extends React.Component{
 
-    render(){
-        return(
-            <div>
-                <p>SOY EL COMPONENTE DE ADRI </p>
+class Bstatic extends React.Component {
+
+    render() {
+        return (
+            <div className="translates" >
+              {/* dentro de las llaves iria el nombre de la ruta en la que este parado el usuario */}
+                <Textb>{}</Textb>
+                <div className="dogt"> <img src="" alt="perro imagen para traslados"></img> </div>
+                <div className="square"></div>
             </div>
         )
     }
 }
 export default Bstatic;
+
+// Este componente es la base del banner que pertenene a  las vistas
+//  de traslados,blog,detalle del producto y detalle de cachorros AM
