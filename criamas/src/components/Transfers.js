a664f5e662ff5f906ffafb565697582e1658aebb
Adriana M <adrianaymontilla@gmail.com>
2019-06-17 14:28:31 -0300

    feat: corazones traslados

--- criamas/src/components/Transfers.js
+++ criamas/src/components/Transfers.js
@@ -17,19 +17,19 @@ class Transfers extends Component {
                 <p>criamas realiza TODA la gestión: desde el primer contacto hasta la entrega de tu cachorro a la compañía aérea, incluyendo las inspecciones sanitarias, el despacho de aduanas y todos los servicios requeridos por nuestros clientes.</p>
                 <div className="viñetas">
                     <div className="left">
-                    <p className="heart">  <i class="fab fa-gratipay">Transporte de animales vivos</i></p>
-                    <p className="heart">  <i class="fab fa-gratipay">Importación/Exportación</i></p>
-                    <p className="heart">  <i class="fab fa-gratipay">Controles en Fronteras (Veterinario y CITES)</i></p>
-                    <p className="heart">  <i class="fab fa-gratipay">Recogida/Entrega a domicilio</i></p>
-                    <p className="heart">  <i class="fab fa-gratipay">Tramitación de la documentación</i></p>
-                    <p className="heart">  <i class="fab fa-gratipay">Visados</i></p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Transporte de animales vivos</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Importación/Exportación</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Controles en Fronteras (Veterinario y CITES)</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Recogida/Entrega a domicilio</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Tramitación de la documentación</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Visados</p>
                     </div>
                     <div className="rigth">
-                        <p className="v">*Disposición de Transportines</p>
-                        <p className="v">*Consultas</p>
-                        <p className="v">*Hotel de animales</p>
-                        <p className="v">*Teléfono 24 hrs. para emergencias</p>
-                        <p className="v">*Servicio VIP</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Disposición de Transportines</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Consultas</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Hotel de animales</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Teléfono 24 hrs. para emergencias</p>
+                    <p className="">  <i className="fab fa-gratipay heart"></i>Servicio VIP</p>
                     </div>
                     <p>Todos nuestros servicios están orientados a conseguir el máximo bienestar y trato respetuoso y amoroso  durante su transporte, desde la recogida en origen hasta la entrega en destino (según servicio).  También podemos ofrecer de un servicio de hoteles para animales, con alojamientos específicos, con control de temperatura según sus necesidades, utilizables en aquellos casos en los que sea necesario efectuar un tránsito o pasar una noche.</p>
                 </div>
