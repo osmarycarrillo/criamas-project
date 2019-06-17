67a381e3b643fc810443a67c0cb2d07b1f549469
Lourdes Rosales <lourdesrosales1997@gmail.com>
2019-06-17 14:26:19 -0300

    style DataContact component

--- criamas/src/components/DataContact.js
+++ criamas/src/components/DataContact.js
@@ -5,23 +5,36 @@ class DataContact extends React.Component {
         return (
             <div className="data-contact">
                 <div>
-                    <i class="fas fa-map-marker-alt"></i>
-                    <p>Dirección</p>
-                    <p className="line-con"></p>
-                    <p>Nicaragua 5596</p> 
+                    <div className="div-ic">
+                        <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <div className="div-dir">
+                        <p>Dirección</p>
+                        <p className="line-con"></p>
+                        <p>Nicaragua 5596</p> 
+                    </div>
                 </div>
                 <div>
-                    <i className="fas fa-mobile-alt"></i>
-                    <p>Tel</p>
-                    <p className="line-con"></p>
-                    <p>+54 9 11 4975-2537 <br/> +54 9 11 3589-8532</p>
+                    <div className="div-ic">
+                        <i className="fas fa-mobile-alt"></i>
+                    </div>
+                    <div className="div-dir">
+                        <p>Teléfono</p>
+                        <p className="line-con"></p>
+                        <p>+54 9 11 4975-2537 <br/> +54 9 11 3589-8532</p>
+                    </div>
                 </div>
                 <div>
-                    <i className="fas fa-envelope"></i>
-                    <p>Mail</p>
-                    <p className="line-con"></p>
-                    <p>lidiasisi@gmail.com</p>
+                    <div className="div-ic">
+                        <i className="fas fa-envelope"></i>
+                    </div>
+                    <div className="div-dir">
+                        <p>Mail</p>
+                        <p className="line-con"></p>
+                        <p>lidiasisi@gmail.com</p>
+                    </div>
                 </div>
+                <div>ACA VA EL MAPAAAAAAAAAAAA!!!!!!!!!!!!!!!!!!!!</div>
             </div>
         )
     }
