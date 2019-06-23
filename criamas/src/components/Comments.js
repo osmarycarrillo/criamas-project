430dd8f2f8911b05f26b73c51f16942b3f579653
mechiventocilla87 <mercedesventocilla@gmail.com>
2019-06-22 21:39:49 -0300

    add comment_component

--- criamas/src/components/Comments.js
+++ criamas/src/components/Comments.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
 import Button from './Button';
+import Testimony from './comments/Testimony';
 
 class Comments extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      comments: Testimony.clientes[0]
+    }
+
+  }
+
+  nextTestimony = () => {
+    const newIndex = this.state.comments.id+1;
+    this.setState({
+      comments: Testimony.clientes[newIndex]
+    })
+  }
+
+  prevTestimony = () => {
+    const newIndex = this.state.comments.id-1;
+    this.setState({
+      comments: Testimony.clientes[newIndex]
+    })
+  }
 
   render() {
+    const {comments} = this.state;
+    const {name, occupation, image, testimony} = comments;
     return (
       <div className="comments">
-          <i class="fas fa-chevron-left"></i>
+        <div className="box_comments">
+          <div className="box_button">
+            <button className="button_comments" onClick={() => this.prevTestimony()} disabled={comments.id === 0}>
+              <i className="fas fa-chevron-left chevron_style"></i>
+            </button>
+          </div>         
           <div className="div-com">
-          <img src="regsf"></img>
-            <h3>Yamila Rodriguez</h3>
-            <p>"fsajchzgsSIDOZCBJNJDWBASCJdbfhjcxascbjkcbz <br/> ehfajsklgaovhcjlgisvhbjalfvhscjfbj"</p>
-            <Button name="Unirme al club" style="join-club"></Button>
+            <div className="box_comillas">
+              <div className="content_comments">
+                <img className="image_cliente" src={image} alt=""></img>
+              </div>
+            </div>              
+              <h3 className="nombre_cliente">{name} <span className="cliente_occupation"> · {occupation}</span></h3>
+              <p className="testimony_cliente">"{testimony}"</p>
+              <div className="button_container">
+               <Button identificator="button_club" name="Unirme al club" styleCss="join-us"/>
+              </div>
           </div>
-          <i class="fas fa-chevron-right"></i>
+          <div className="box_button">
+            <button className="button_comments" onClick={() => this.nextTestimony()} disabled={comments.id === Testimony.clientes.length-1}>
+                <i className="fas fa-chevron-right chevron_style"></i>
+            </button>
+          </div>            
+        </div>         
       </div>
     );
   }
---
+++ criamas/src/components/comments/Testimony.js
@@ -0,0 +1,32 @@
+import Cliente01 from '../../img_photos/testimonio01.jpg';
+import Cliente02 from '../../img_photos/testimonio02.jpg';
+import Cliente03 from '../../img_photos/testimonio03.jpg';
+
+const Testimony = {
+    clientes: [
+        {
+            id: 0,
+            name: 'Yamila Rodriguez',
+            occupation: 'Secretaria',
+            image: Cliente01,
+            testimony: "Nonummyeuismod tincidunt ut lt, consectetuer adipiscing e lit, seddiam nonummyeuismod tincidunt ut lt dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut"
+        },
+        {
+            id: 1,
+            name: 'Agustín Gomez',
+            occupation: 'Arquitecto',
+            image: Cliente02,
+            testimony: "Lorem ipsum dolor sit amet, consectetuer adipiscing e lit, seddiam nonummyeuismod tincidunt ut lt dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut"
+        },
+        {
+            id: 2,
+            name: 'Candela Sánchez',
+            occupation: 'Traductora',
+            image: Cliente03,
+            testimony: "Consectetuer adipiscing e lit, consectetuer adipiscing e lit, seddiam nonummyeuismod tincidunt ut lt dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut"
+        }
+        
+    ]
+};
+
+export default Testimony;
