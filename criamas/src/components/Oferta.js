5a58412485cf98680088eaf40899ac7f8b216871
mechiventocilla87 <mercedesventocilla@gmail.com>
2019-06-22 21:32:59 -0300

    add  changes_oferta

--- criamas/src/components/Oferta.js
+++ criamas/src/components/Oferta.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
 import DataOferta from './oferta/DataOferta';
 
-// const Oferta = ({DataOferta}) => {
-// 	const {id, description, button, image, raza} = property;
-// 	return (
-// 		<div>
-//             <p>{}</p>
-// 		</div>
-// 	);
-// }
-
 class Oferta extends Component {
 	constructor(props){
 	  super(props);
@@ -21,10 +12,24 @@ class Oferta extends Component {
 	render() {
 	  const {property} = this.state;
 	  return (
-		<div className='box_oferta'>
-		  <p>{property.title}</p>
-		</div>
-	  )
+			<div className='box_oferta'>
+				<div className="box_center_oferta">
+					<div className="box_first">
+					  <h3 className="title_oferta">{property.title}</h3>
+					  <p className="subtitle_oferta">{property.tiempo}</p>
+					</div>
+					<div className="box_second">
+						<div className="content_image">                      
+						  <img className="img_oferta" src={property.image} alt=""/>
+                        </div>
+					</div>
+					<div className="box_third">
+					  <h3 className="desc">{property.descuento}</h3>
+					  <p className="products">{property.productos}</p>
+					</div>
+				</div>
+			</div>
+	    )
 	}
 }
 
