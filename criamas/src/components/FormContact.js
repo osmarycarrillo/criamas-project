3c4f23be4135b611d8b05f2fe9b71992e316707e
Osmary Carrillo <osmarycarrillo@gmail.com>
2019-06-25 00:29:51 -0300

    Feat: create tag label ang delete value of input

--- criamas/src/components/FormContact.js
+++ criamas/src/components/FormContact.js
@@ -5,12 +5,16 @@ class FormContact extends React.Component {
     render() {
         return (
             <div className="form-contact">
-                <input type="text" value="Nombre"></input>
-                <input type="text" value="Email"></input>
-                <input type="text" value="Asunto"></input>
-                <input className="input-msj" type="text" value="Mensaje"></input>
+                <label htmlFor="Nombre">Nombre:</label>
+                <input type="text" name="Nombre" ></input>
+                <label htmlFor="Email">Email:</label>
+                <input type="text" name="Email"></input>
+                <label htmlFor="Asunto">Asunto:</label>
+                <input type="text" name="Asunto"></input>
+                <label htmlFor="Mensaje">Mensaje:</label>
+                <input className="input-msj" type="text" name="Mensaje"></input>
                 <div className="container_contact">
-                  <Button name="Enviar" styleCss="button-con"></Button>
+                    <Button name="Enviar" styleCss="button-con"></Button>
                 </div>
             </div>
         )
