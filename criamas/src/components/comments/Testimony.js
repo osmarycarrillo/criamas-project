eed6e9a0a4ebd2050f56ebe433689dad0fa4faae
Adriana M <adrianaymontilla@gmail.com>
2019-06-26 16:36:10 -0300

    feat: testimonios

--- criamas/src/components/comments/Testimony.js
+++ criamas/src/components/comments/Testimony.js
@@ -9,7 +9,7 @@ const Testimony = {
             name: 'Yamila Rodriguez',
             occupation: 'Secretaria',
             image: Cliente01,
-            testimony: "Gracias Criamas, me lleve una gran sorpresa al recibir a mi cachorro con su kit de bienvenida, se nota que cuidan cada detalle para poder hacer el cuidado mas adecuado a nueatras mascotas."},
+            testimony: "Gracias Criamas, me lleve una gran sorpresa al recibir a mi cachorro con su kit de bienvenida,cuidan cada detalle para poder hacer el cuidado mas adecuado a nueatras mascotas."},
         {
             id: 1,
             name: 'Agustín Gomez',
@@ -22,7 +22,7 @@ const Testimony = {
             name: 'Candela Sánchez',
             occupation: 'Traductora',
             image: Cliente03,
-            testimony: "Antes de llegar con ustedes para encontrarme con mi nuevo amor estaba llena de dudas, ustedes las disiparon todas, lo mejor es que ahora formo parte de una comunidad de dueños de cachorros que esta en la misma sintonia"
+            testimony: "Antes de llegar al encuentro de mi nuevo amor estaba llena de dudas, ustedes las disiparon todas, lo mejor es que ahora formo parte de una comunidad de dueños de cachorros que esta en la misma sintonia"
         }
         
     ]
