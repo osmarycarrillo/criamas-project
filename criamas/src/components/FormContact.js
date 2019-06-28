32d8f862599d7f08ceff05cc26a2ff284321cdbe
Osmary Carrillo <osmarycarrillo@gmail.com>
2019-06-28 00:00:19 -0300

    creacion de label y modificacion input

--- criamas/src/components/FormContact.js
+++ criamas/src/components/FormContact.js
@@ -5,10 +5,14 @@ class FormContact extends React.Component {
     render() {
         return (
             <div className="form-contact">
-                <input type="text" value="Nombre"></input>
-                <input type="text" value="Email"></input>
-                <input type="text" value="Asunto"></input>
-                <input className="input-msj" type="text" value="Mensaje"></input>
+                <label htmlFor="Nombre">Nombre:</label>
+                <input type="text" name="Nombre" ></input>
+                <label htmlFor="Email">Email:</label>
+                <input type="text" name="Email"></input>
+                <label htmlFor="Asunto">Asunto:</label>
+                <input type="text" name="Asunto"></input>
+                <label htmlFor="Mensaje">Mensaje:</label>
+                <input className="input-msj" type="text" name="Mensaje"></input>
                 <div className="container_contact">
                   <Button name="Enviar" styleCss="button-con"></Button>
                 </div>
