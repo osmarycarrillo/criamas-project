85ad69ee061806c4cced53b374f9e3533dbc8035
mechiventocilla87 <mercedesventocilla@gmail.com>
2019-06-28 16:28:08 -0300

    change_buttonshop

--- criamas/src/components/Button.js
+++ criamas/src/components/Button.js
@@ -3,18 +3,6 @@ import { Link } from "react-router-dom";
 import '../stylesheet/App.css';
 
 const Button = (props) => {
-<<<<<<< HEAD
-    
-    return (
-        <div className='container_box_component'>
-            <Link to={props.url}>
-                <button id={props.identificator} className={props.styleCss}>
-                    {props.name}
-                </button>
-            </Link>
-        </div>
-    );
-=======
    return (
        <div className='container_box_component'>
          <Link to={props.url}>
@@ -26,7 +14,6 @@ const Button = (props) => {
            </Link>
        </div>
    );
->>>>>>> b9d619d24e9617ec977cec517528ed9e69427988
 }
 
 export default Button;
